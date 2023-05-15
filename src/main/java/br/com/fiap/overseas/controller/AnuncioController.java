bcb86f8bdf6b7fa24dea789fba07f5af5d23d0ce
Mayara Amorim <101272224+Mayara-Amorim@users.noreply.github.com>
2023-05-15 16:52:39 -0300

    Implementando metodos Listar Tudo e Buscar por Id: Anuncios

--- src/main/java/br/com/fiap/overseas/controller/AnuncioController.java
+++ src/main/java/br/com/fiap/overseas/controller/AnuncioController.java
@@ -3,9 +3,11 @@ package br.com.fiap.overseas.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +24,14 @@ public class AnuncioController {
 	@GetMapping
 	public ResponseEntity <List<AnuncioModel>> getAll(){
 		return ResponseEntity.ok(aR.findAll());
-		
+	}
+	
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<AnuncioModel> getById(@PathVariable Long id){
+		return aR.findById(id)
+		.map(resposta -> ResponseEntity.ok(resposta))
+		.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
 
 }
