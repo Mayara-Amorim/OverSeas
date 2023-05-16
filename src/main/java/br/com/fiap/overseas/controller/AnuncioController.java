491977ee715820fb383fa4f903d1e21f872725b1
Mayara Amorim <101272224+Mayara-Amorim@users.noreply.github.com>
2023-05-16 10:23:27 -0300

    Implementando metodo criar controller anuncios

--- src/main/java/br/com/fiap/overseas/controller/AnuncioController.java
+++ src/main/java/br/com/fiap/overseas/controller/AnuncioController.java
@@ -8,11 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.overseas.model.AnuncioModel;
 import br.com.fiap.overseas.repository.AnuncioRepository;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/anuncios")
@@ -33,5 +36,18 @@ public class AnuncioController {
 		.map(resposta -> ResponseEntity.ok(resposta))
 		.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity <List<AnuncioModel>> getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(aR.findAllByTituloContainingIgnoreCase(titulo));
+	}
 
+	@PostMapping
+	public ResponseEntity<AnuncioModel> post(@Valid @RequestBody AnuncioModel aM){
+		return ResponseEntity.status(HttpStatus.CREATED).body(aR.save(aM));
+	}
+	
+	
+	
+	
+	
 }
