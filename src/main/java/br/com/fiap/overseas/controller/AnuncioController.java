8e309a95b5e99cc2de8f7358eb1a0df9de7f7fcd
Mayara Amorim <101272224+Mayara-Amorim@users.noreply.github.com>
2023-05-16 10:59:12 -0300

    Implementando metodo DELETAR controller anuncios

--- src/main/java/br/com/fiap/overseas/controller/AnuncioController.java
+++ src/main/java/br/com/fiap/overseas/controller/AnuncioController.java
@@ -1,18 +1,22 @@
 package br.com.fiap.overseas.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.fiap.overseas.model.AnuncioModel;
 import br.com.fiap.overseas.repository.AnuncioRepository;
@@ -54,6 +58,15 @@ public class AnuncioController {
 				.map(resp-> ResponseEntity.status(HttpStatus.OK)
 				.body(aR.save(aM)))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+	Optional<AnuncioModel> anuncio =	aR.findById(id);
+		if(anuncio.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		aR.deleteById(id);
 	}
 	
 	
