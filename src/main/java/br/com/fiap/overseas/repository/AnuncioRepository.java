a49c6fbeb78593fcb2ed0f7b7128a6846e8a5cd8
Mayara Amorim <101272224+Mayara-Amorim@users.noreply.github.com>
2023-05-16 09:36:39 -0300

    Criando query method interface anuncios

--- src/main/java/br/com/fiap/overseas/repository/AnuncioRepository.java
+++ src/main/java/br/com/fiap/overseas/repository/AnuncioRepository.java
@@ -1,10 +1,14 @@
 package br.com.fiap.overseas.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.fiap.overseas.model.AnuncioModel;
 @Repository
 public interface AnuncioRepository extends JpaRepository<AnuncioModel, Long>{
+	public List<AnuncioModel> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 
 }
