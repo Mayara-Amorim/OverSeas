297bef997e23ce316f14b0213d77f6df3b646e4b
Mayara Amorim <101272224+Mayara-Amorim@users.noreply.github.com>
2023-05-18 11:13:14 -0300

    Query Method buscar usuario

--- src/main/java/br/com/fiap/overseas/repository/UsuarioRepository.java
+++ src/main/java/br/com/fiap/overseas/repository/UsuarioRepository.java
@@ -1,10 +1,13 @@
 package br.com.fiap.overseas.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.fiap.overseas.model.UsuarioModel;
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
+	public Optional<UsuarioModel> findByUsuario(String usuario);
 
 }
